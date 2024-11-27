99fabbfc0c357aeb7d1cdb0903b9f0f4d37ef494
DedeepyaGelli <121683311+DedeepyaGelli@users.noreply.github.com>
2024-11-27 19:02:41 +0530

    push

--- db.js
+++ db.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 const mysql2 = require('mysql2');
 
 const connection = mysql2.createPool({
-    // host: process.env.DB_URL,            
+    host: process.env.DB_URL,       
+    port: process.env.DB_PORT,       
     user: process.env.DB_USERNAME,   
     password: process.env.DB_PASSWORD, 
-    database: process.env.DBNAME,
-    port: process.env.DB_PORT  
-
+    database: process.env.DBNAME    
 }).promise();
 
 module.exports = connection;
